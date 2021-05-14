970393224404f140c5ecc6dcc22cb2bf7b92bdd1
Luis Martinez <luis_mmartinez@live.com>
2021-05-13 23:57:53 -0400

    setting up api in db:seed file

--- Gemfile
+++ Gemfile
@@ -25,6 +25,10 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
+gem 'dotenv-rails'
+gem 'rest-client'
+gem 'json'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
@@ -34,6 +38,7 @@ group :development, :test do
 end
 
 group :development do
+  gem 'pry'
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
   gem 'web-console', '>= 4.1.0'
   # Display performance information such as SQL time and flame graphs for each request in your browser.
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+binding.pry
+
+# response = RestClient.get("https://api.spacexdata.com/v4/launches")
+# launches = JSON.parse(response)
+# first_launch = launches.first
+
+# response = RestClient.get("url/auth= #{ENV["KEY"]}",
+#     headers: ENV['KEY']
+#     x-api-key: ENV['KEY']
+# )
+
+# brreds_array = JSON.parse(response)
+
+carbon_response = RestClient.get("https://www.carboninterface.com/api/v1/auth",
+  Authorization: Bearer ENV['CARBON_KEY']"
+)
+
+
