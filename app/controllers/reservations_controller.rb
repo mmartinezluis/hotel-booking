bbee46d9c9adbbc31b6ad10400b1ad5af1db1176
Luis Martinez <luis_mmartinez@live.com>
2021-06-01 17:17:18 -0400

    add control flow to show action of reservations controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -5,8 +5,14 @@ class ReservationsController < ApplicationController
   end
 
   def show
-    @single_reservation = User.first.reservations.find_by(id: params[:id]) 
-    @hotel = @single_reservation.hotel
-    render :'hotels/show.html.erb' 
+    if !User.first.reservations.exists?(params[:id])
+      flash[:msg] = "Reservation not found"
+      redirect_to reservations_path 
+    else
+      @single_reservation = User.first.reservations.find_by(id: params[:id]) 
+      @hotel = @single_reservation.hotel
+      render :'hotels/show.html.erb' 
+    end
   end
+
 end
