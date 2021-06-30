13071d5a6dfa3bb56993f18b1a930ca21b34a9fb
Luis Martinez <luis_mmartinez@live.com>
2021-06-30 14:55:43 -0400

    defined most_popular class method for hotel model to return the 3 hotels with most reservations

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -5,12 +5,13 @@ class Hotel < ApplicationRecord
   belongs_to :city
 
   include Normalizable::Parse
-
-  scope :most_popular, -> {order(reservation_ids: :asc)}
+  # scope :most_popular, -> {order(reservation_ids: :asc).joins(:reservations).order(checkout_date: :desc)}
+  # scope :most_popular, -> {order("LENGTH(reservation_ids) desc")}
+  # scope :most_popular, -> {order(reservation_ids: :asc)}
   
   # Since a hotel comes with a reservation and a room, hotels are built with both objects nested
   def self.build_hotel(hotel_hash, user_id)
-    city = City.find_or_create_by(code: hotel_hash["hotel"]["cityCode"])  # , name: hotel_hash["hotel"]["address"]["cityName"]
+    city = City.find_or_create_by(code: hotel_hash["hotel"]["cityCode"])  #, name: hotel_hash["hotel"]["address"]["cityName"]
     hotel = Hotel.find_by(hotelId: hotel_hash["hotel"]["hotelId"])
     # If the hotel is already in the database, do not build a new hotel (skip the below block)
     unless hotel 
@@ -18,14 +19,11 @@ class Hotel < ApplicationRecord
       hotel.city_id = city.id
       hotel.hotelId = hotel_hash["hotel"]["hotelId"]
       hotel.citycode = hotel_hash["hotel"]["cityCode"]
-      # hotel.name = hotel_hash["hotel"]["name"]
-      hotel.name = hotel.general_parsing(hotel_hash["hotel"]["name"])
+      hotel.name = hotel.general_parsing(hotel_hash["hotel"]["name"])     # 'general_parsing' method comes from the Normalizable module
       hotel.latitude = hotel_hash["hotel"]["latitude"]
       hotel.longitude = hotel_hash["hotel"]["longitude"]
-      # hotel.address = hotel_hash["hotel"]["address"]
-      hotel.address = hotel.parse_address(hotel_hash["hotel"]["address"])
+      hotel.address = hotel.parse_address(hotel_hash["hotel"]["address"])   # 'parse_address' method comes from the Normalizable module
       hotel.description = hotel_hash["hotel"]["description"]["text"] unless !hotel_hash["hotel"].keys.include?("description")         # Some hotels do not inlcude a description
-      # hotel.amenities = hotel_hash["hotel"]["amenities"].take(5).join(", ") unless !hotel_hash["hotel"].keys.include?("amenities")    # Some hotels do not include amenities
       hotel.amenities = hotel.general_parsing(hotel_hash["hotel"]["amenities"].take(5).join(", ")) unless !hotel_hash["hotel"].keys.include?("amenities")    # Some hotels do not include amenities
     end
     #  Build nested reservation for hotel 
@@ -47,7 +45,7 @@ class Hotel < ApplicationRecord
         bedtype: hotel_hash["offers"][0]["room"]["typeEstimated"]["bedType"]        # King, ect.
       )
     else
-    # If the hotel_hash does not include the "TypeEstimated" key, Build a rooom with one attribute only
+    # If the hotel_hash does not include the "TypeEstimated" key, build a rooom with one attribute only
       new_room = new_reservation.build_room(
         category: hotel_hash["offers"][0]["room"]["description"]["text"].split("\n")[1]
       )
@@ -55,6 +53,12 @@ class Hotel < ApplicationRecord
     # Return the hotel with its nested reservation and room
     # The hotel is not persisted to database unless a reservatino is made; see hotels_controller 'reserve' method
     hotel
+  end
+
+  def self.most_popular
+    Hotel.all.sort do |a,b|
+      b.reservation_ids.length <=> a.reservation_ids.length
+    end.take(3)#.includes(:reservations).order(checkin_date: :desc)
   end
   
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,7 +3,8 @@ class Reservation < ApplicationRecord
   belongs_to :user
   has_one :room
   has_one :review
-  # accepts_nested_attributes_for :room
+  
+  # Method used in the select fields of the new review form 
   def hotel_and_checkin
     "#{self.hotel.name}, check-in: #{self.checkin_date}"
   end
