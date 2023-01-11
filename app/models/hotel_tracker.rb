7e58df549cac031bd1a6e74de9b5a674272f52bc
Luis Martinez <luis_mmartinez@live.com>
2023-01-11 17:09:29 -0500

    refactor HotelTracker module

--- app/models/hotel_tracker.rb
+++ app/models/hotel_tracker.rb
@@ -2,10 +2,38 @@
 module HotelTracker
     @@most_popular_ids = []
 
+    class ReservationsPriorityQueue
+        attr_accessor :map, :values
+
+        def initialize(reservations = [])
+            return if reservations.class != Array
+            @map = {}
+            @values = []
+            self.build_queue(reservations) if reservations.length > 0
+        end
+
+        def build_queue(reservations)
+            # reservations should be sorted already by checkin date descending 
+            reservations.each do |re|
+                prev_re = map[re.hotel_id]    
+                map[re.hotel_id] = re if !prev_re
+            end
+            map.keys
+        end
+
+        # for later, write method and data structures to add a reservation and keep hotel ids
+        # sorted by reservation checkin date descending
+        # Potential solution: use a heap and a hashmap; the heap will store the hotel ids;
+        # the hash map will store the hotel id as key and the hotel index from heap as value
+        def add(value)
+        end
+    end
+
     # Maintains user hotels sorted by reservations checkin date descending
     class ForUser
-        attr_accessor 
-        def initialize()
+        attr_accessor :hotel_ids
+        def initialize(reservations)
+            @hotel_ids = HotelTracker::ReservationsPriorityQueue.new(reservations)
         end
     end
 
@@ -13,24 +41,5 @@ module HotelTracker
     class ForApp
     end
 
-    class ReservationsPriorityQueue
-        attr_accessor :reservation_ids, :list, :queue
 
-        def initialize(reservations = [])
-            return if reservations.class != Array
-            @reservation_ids = reservations.length > 0 ? self.build_queue(reservations) : []
-            @list = {}
-        end
-
-        def build_queue(reservations)
-            reservations.each do |re|
-                # if list[re.hotel_id]
-            end
-
-        end
-
-        def add(value)
-        end
-
-    end
 end
